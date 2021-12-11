3ecf25c5b45acd7720420cf73128c63fd5e5c3a4
Ali Adel <aliadel56000@gmail.com>
2021-12-11 11:23:30 +0200

    Add 404 page

--- src/modules/NotFound/index.jsx
+++ src/modules/NotFound/index.jsx
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import Classes from './index.module.css';
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div id={Classes.notfound} className="bg-light">
+      <div className={Classes.notfound}>
+        <div className={Classes.notfound404}>
+          <h1>
+            4<span>0</span>4
+          </h1>
+        </div>
+
+        <h2>We can't find that page</h2>
+
+        <p className="text-info">
+          We're fairly sure that page used to be here, but seems to have gone
+          missing. We do apologise on it's behalf.
+        </p>
+
+        <Link to="/">Home</Link>
+      </div>
     </div>
   );
 };
