eae470a4beaf2671f5395fe7914b500dcc305faf
Ali Adel <aliadel56000@gmail.com>
2021-12-11 17:50:31 +0200

    Add homepage

--- src/modules/Home/index.jsx
+++ src/modules/Home/index.jsx
@@ -1,7 +1,53 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+import Classes from './index.module.css';
+import HeroImage from '../../images/hero.jpg';
+
 const Home = () => {
-  return (
+  const authenticated = useSelector((state) => state.auth.authenticated);
+
+  let content = (
     <div>
-      <h1>Home</h1>
+      <h2>Want to enroll in the best courses to help in your improvment?</h2>
+
+      <h3>
+        <Link className={Classes.link} to="/auth/signup">
+          Signup
+        </Link>{' '}
+        now to get access to the best courses this world has to offer!
+      </h3>
+
+      <h6>
+        Already a member?{' '}
+        <Link className={Classes.link} to="/auth/login">
+          Login
+        </Link>{' '}
+        Now!
+      </h6>
+    </div>
+  );
+
+  if (authenticated) {
+    content = (
+      <h2>
+        Go to your{' '}
+        <Button variant="outline-info" size="sm">
+          <Link className="btn outline" to="/courses">
+            courses
+          </Link>
+        </Button>{' '}
+        to catch up!
+      </h2>
+    );
+  }
+
+  return (
+    <div className={Classes.wrapper}>
+      <img src={HeroImage} alt="Hero" />
+
+      {content}
     </div>
   );
 };
